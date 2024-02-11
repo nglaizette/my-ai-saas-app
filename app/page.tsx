aab30985eca23468c7b070a071b3c1b35ceb9a27
NicoG <winnipeg.francois@gmail.com>
2024-02-11 17:20:05 +0100

    Ajout d'un bouton provenant de Shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
+    <>
     <p className="text-6xl text-green-500">Hello Ai Saas</p>
-  );
+    <Button variant="destructive">Click me</Button>
+    </>
+    );
 }
