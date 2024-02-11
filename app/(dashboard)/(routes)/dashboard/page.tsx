295e04e36d69f907539b503a830adcd8847e3eb9
NicoG <winnipeg.francois@gmail.com>
2024-02-11 23:09:37 +0100

    Ajout Authentification

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
-    <>
-    <p>Dashboard page (Protected)</p>
-    <p className="text-6xl text-green-500">Hello Ai Saas</p>
-    <Button variant="destructive">Click me</Button>
-    </>
+    <div>
+      <p>Dashboard page (Protected)</p>
+      <p className="text-6xl text-green-500">Hello Ai Saas</p>
+      <Button variant="destructive">Click me</Button>
+      <div>
+        <UserButton afterSignOutUrl="/"/>
+      </div>
+    </div>
     );
 }
