6fe9db90f9ecdc9e27e62e830373e4eb2182b1d1
NicoG <winnipeg.francois@gmail.com>
2024-02-11 18:23:57 +0100

    Création de "routes groups" page autorisée ou non

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function DashboardPage() {
   return (
     <>
+    <p>Dashboard page (Protected)</p>
     <p className="text-6xl text-green-500">Hello Ai Saas</p>
     <Button variant="destructive">Click me</Button>
     </>
