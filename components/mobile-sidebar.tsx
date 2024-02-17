5efe2d3012ca7c81da024e2485f6d785d7d9d2d1
NicoG <winnipeg.francois@gmail.com>
2024-02-17 17:34:47 +0100

    Création mobile-sidebar composant

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+const MobileSidebar = () => {
+	return  (
+		<Button variant="ghost" size="icon" className="md:hidden">
+			<Menu />
+		</Button>
+	);
+}
+
+export default MobileSidebar
+
