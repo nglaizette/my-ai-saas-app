55aed1bdee874d206850352ba1b6db2ea8aaa0ce
NicoG <winnipeg.francois@gmail.com>
2024-03-05 23:11:03 +0100

    Construction de l'api music génération

--- app/(dashboard)/(routes)/music/constants.ts
+++ app/(dashboard)/(routes)/music/constants.ts
@@ -3,6 +3,6 @@ import * as z from "zod"
 export const fromSchema = z.object(
 	{
 		prompt: z.string().min(1, {
-			message: "Prompt is required"
+			message: "Music prompt is required"
 		}),
 	});
