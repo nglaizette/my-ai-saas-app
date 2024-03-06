b6f0406d670575057a62d9a6aaa2812b8424a71d
NicoG <winnipeg.francois@gmail.com>
2024-03-06 22:31:38 +0100

    Implémentation de l'api pour la video

--- app/(dashboard)/(routes)/video/constants.ts
+++ app/(dashboard)/(routes)/video/constants.ts
@@ -3,6 +3,6 @@ import * as z from "zod"
 export const fromSchema = z.object(
 	{
 		prompt: z.string().min(1, {
-			message: "Music prompt is required"
+			message: "Video prompt is required"
 		}),
 	});
